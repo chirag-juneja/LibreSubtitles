22e8a134dcbd687dd9d90878d580483605330a87
Chirag Juneja <chiragjuneja6@gmail.com>
2025-09-11 22:35:29 +0530

    whisper STT

--- libresubtitles/core/stt.py
+++ libresubtitles/core/stt.py
@@ -1,13 +1,22 @@
 import whisper
 import os
-from libresubtitles.core.global import MODEL_DIR
+from libresubtitles.core.globals import MODEL_DIR
 
 
 def load_model(model_name="tiny"):
     os.makedirs(MODEL_DIR, exist_ok=True)
     os.environ["XDG_CACHE_HOME"] = MODEL_DIR
-
-    model = whisper.load_model(model_name)
+    model = whisper.load_model(model_name, device="cpu")
     return model
 
-load_model()
+
+def transcribe_audio(file_path: str) -> str:
+    model = load_model()
+    result = model.transcribe(file_path, fp16=False)
+    print(result["segments"])
+    return result["text"]
+
+
+if __name__ == "__main__":
+    text = transcribe_audio("./data/sample.wav")
+    print(text)
